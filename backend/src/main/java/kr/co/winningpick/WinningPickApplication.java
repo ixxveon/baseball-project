0594b7b66cc47538b0741588a5f7b5aabc381b38
ixxveon <igayeon322s@gmail.com>
2026-08-18 19:54:56 +0900

    feat: Member 엔티티 및 ProviderType, JPA Auditing 설정 추가

--- backend/src/main/java/kr/co/winningpick/WinningPickApplication.java
+++ backend/src/main/java/kr/co/winningpick/WinningPickApplication.java
@@ -2,8 +2,10 @@ package kr.co.winningpick;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class WinningPickApplication {
 
     public static void main(String[] args) {
