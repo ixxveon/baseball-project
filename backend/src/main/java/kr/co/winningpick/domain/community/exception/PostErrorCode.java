da4d5b807b39c6a9ae0c52bc5f0ca7a19113064e
ixxveon <igayeon322s@gmail.com>
2026-09-14 15:34:18 +0900

    feat: PostErrorCode에 권한 없음 케이스 추가

--- backend/src/main/java/kr/co/winningpick/domain/community/exception/PostErrorCode.java
+++ backend/src/main/java/kr/co/winningpick/domain/community/exception/PostErrorCode.java
@@ -9,7 +9,8 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum PostErrorCode implements ErrorCode {
 
-    POST_NOT_FOUND(HttpStatus.NOT_FOUND, "게시글을 찾을 수 없습니다");
+    POST_NOT_FOUND(HttpStatus.NOT_FOUND, "게시글을 찾을 수 없습니다"),
+    FORBIDDEN(HttpStatus.FORBIDDEN, "본인 게시글만 수정/삭제할 수 있습니다");
 
     private final HttpStatus status;
     private final String message;
