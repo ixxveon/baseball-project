711e9670ec955ac676ce6348b2a2649c090a6f36
ixxveon <igayeon322s@gmail.com>
2026-09-18 14:00:37 +0900

    feat: 카카오 OAuth2 사용자 조회/자동가입 서비스 추가 및 SecurityConfig 연결

---
+++ backend/src/main/java/kr/co/winningpick/domain/member/oauth/KakaoOAuth2UserService.java
@@ -0,0 +1,37 @@
+package kr.co.winningpick.domain.member.oauth;
+
+import kr.co.winningpick.domain.member.entity.Member;
+import kr.co.winningpick.domain.member.repository.MemberRepository;
+import kr.co.winningpick.domain.member.type.ProviderType;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+
+@Service
+@RequiredArgsConstructor
+public class KakaoOAuth2UserService extends DefaultOAuth2UserService {
+
+    private final MemberRepository memberRepository;
+
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+        OAuth2User oAuth2User = super.loadUser(userRequest);
+
+        String socialId = String.valueOf(oAuth2User.getAttribute("id"));
+
+        Map<String, Object> kakaoAccount = oAuth2User.getAttribute("kakao_account");
+        Map<String, Object> profile = (Map<String, Object>) kakaoAccount.get("profile");
+        String email = (String) kakaoAccount.get("email");
+        String nickname = (String) profile.get("nickname");
+
+        memberRepository.findBySocialIdAndProvider(socialId, ProviderType.KAKAO)
+                .orElseGet(() -> memberRepository.save(Member.createSocialMember(email, nickname, ProviderType.KAKAO, socialId)));
+
+        return oAuth2User;
+    }
+}
